b95753a70da2d7141feb5f6825740ea47e137ed2
Dinesh Sai <namburudineshsai@gmail.com>
2022-10-16 23:56:58 +0530

    changed address2 varchar from 50 to 200

--- example/changelogs/samplechangelog.h2.sql
+++ example/changelogs/samplechangelog.h2.sql
@@ -6,7 +6,7 @@ create table person (
     id int primary key,
     name varchar(50) not null,
     address1 varchar(100),
-    address2 varchar(50),
+    address2 varchar(500),
     city varchar(30)
 )
 
